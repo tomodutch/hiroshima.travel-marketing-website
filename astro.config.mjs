d3683b12a0a395c80450a5034e108a59280660ed
tomodutch <4613944+tomodutch@users.noreply.github.com>
2025-08-13 08:51:45 +0900

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://hiroshima.travel',
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
